e66cffea2df3a3452afbaa8e12261bae2df16689
武田喜一 <takedakiichi@takedakiichinoMac-mini.local>
2025-04-21 14:17:45 +0900

    feat: add sitemap generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://example.com', // Replace with your site's URL
+  generateRobotsTxt: true, // (Optional) Generate robots.txt
+  // ...other options
+} 
