4141ae12c7afd175ec1fbec4d6fd56ce3f9991ca
武田喜一 <takedakiichi@takedakiichinoMac-mini-2.local>
2025-06-12 16:28:19 +0900

    feat: Redirect root '/' to '/ssw'

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ const nextConfig = {
   basePath: '/ssw',
   assetPrefix: '/ssw',
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/ssw',
+        permanent: false,
+        basePath: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig; 
